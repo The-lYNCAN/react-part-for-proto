9ed0881993a582b59bffcff2a202aeadec60540b
kushagar chodary 11 <chodary11ki@hotmail.com>
2021-01-05 12:20:36 +0530

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {  }
+
+  name = () => {
+    let myjson = null
+    fetch('http://localhost:8000/index')
+    .then(res => {
+      console.log(res.json)
+      return res.json()
+    })
+    .then(json => {
+      console.log(json.username);
+      myjson = json
+    })
+    return myjson
+  }
+
+  render() { 
+    return ( 
+      <React.Fragment>
+      <h1>Hi</h1>
+      {this.name()}
+      </React.Fragment>
+     );
+  }
 }
-
+ 
 export default App;
